9d415c56d12be471a72cd6aee54a376d3a552422
kee-000 <64545575+kee-000@users.noreply.github.com>
2020-07-29 15:07:50 -0400

    Updated app
    Added background and updated CSS
    Added new Bootstrap element
    Added precipitation element
    Added forecast

--- script.js
+++ script.js
@@ -1,18 +1,21 @@
-let currentTime = new Date();
-let dateElement = document.querySelector("#date");
-let hours = currentTime.getHours();
-let minutes = currentTime.getMinutes();
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
+let currentTime = new Date();
+let hours = currentTime.getHours();
+let minutes = currentTime.getMinutes();
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
 ];
-let day = days[currentTime.getDay()];
-dateElement.innerHTML = `${day} ${hours}:${minutes}`;
+
+let day = days[currentTime.getDay()];
+let [month, date, year] = new Date().toLocaleDateString().split("/");
+let dateElement = document.querySelector("#date");
+dateElement.innerHTML = `${day} ${month} ${date}, ${year} ${hours}:${minutes}`;
 
 function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
@@ -23,6 +26,7 @@ function displayTemperature(response) {
   let descriptionElement = document.querySelector("#description");
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
+  let forecastElement = document.querySelector("#forecast");
   let iconElement = document.querySelector("#icon");
 
   celsiusTemperature = response.data.main.temp;
@@ -32,7 +36,7 @@ function displayTemperature(response) {
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
   windElement.innerHTML = Math.round(response.data.wind.speed);
-
+  
   iconElement.setAttribute(
     "src",
     `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
@@ -40,10 +44,30 @@ function displayTemperature(response) {
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
 
+function displayForecast(response) {
+let forecastElement = document.querySelector("#forecast");
+let forecast = response.data.list[0];
+console.log(forecast);
+
+forecastElement.innerHTML = `
+<div class="col-2">
+<h6>${forecast.dt}</h6>
+<img src="http://openweathermap.org/img/wn/${forecast.weather[0].icon}@2x.png"
+alt=""
+/>
+<div class="weather-forecast-temperature">
+<strong>${Math.round(forecast.main.temp_max)}°</strong> ${Math.round(forecast.main.temp_min)}°
+</div>
+`;
+}
+
 function getCity(city) {
   let apiKey = "9043642421a658c4c8cf827658c62393";
   let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${apiKey}&units=metric`;
   axios.get(apiUrl).then(displayTemperature);
+
+  apiUrl = `https://api.openweathermap.org/data/2.5/forecast?q=${city}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function search(event) {
