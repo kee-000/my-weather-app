32c4099191c98b5d391eef69793e9c1cec1cd77d
kee-000 <64545575+kee-000@users.noreply.github.com>
2020-07-31 13:15:54 -0400

    Updated app
    Removed precipitation li from html
    Changed "search" to "getCity" in js

--- script.js
+++ script.js
@@ -104,4 +104,4 @@ fahrenheitLink.addEventListener("click", displayFahrenheitTemperature);
 let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
-search("Toronto");
+getCity("Toronto");
